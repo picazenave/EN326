8342e21bcb9cd00f79c5290311aaf1e426b321a1
Pierre Cazenave <47040310+picazenave@users.noreply.github.com>
2023-10-06 14:39:26 +0200

    decode done

--- main.cpp
+++ main.cpp
@@ -16,33 +16,51 @@ DigitalIn button(BUTTON1);
 #error BUTTON1 not defined
 #endif
 
+#define READ_BYTE_LEN 9
+
 I2C i2c(P1_I2C_SDA, P1_I2C_SCL);
 const char addr = (0x62 << 1);
 
 int counter = 0;
 
-void decodeSCD4(char *data, int *CO2ppm, float *temp, float *RH)
+void decode_SCD4_read(char *data, uint16_t *CO2ppm, float *temp, float *RH)
 {
-    *CO2ppm=data[0];
+    *CO2ppm = (data[0] << 8) + data[1];
+    uint16_t a = (data[3] << 8) + data[4];
+    // FIXME offset
+    int offset = -45;
+    *temp = (float)offset + (float)175 * (a / (float)(1 << 16));
+    a = (data[6] << 8) + data[7];
+    *RH = (float)100 * (a / (float)(1 << 16));
 }
 
 int main()
 {
     while (true)
     {
+        // TODO lock bus
+        // TODO check CRC
+        // TODO U16_TO_BYTE_ARRAY pour les commandes
+        // TODO BYTE_TO_U16
         char start_command[2] = {0x21, 0xb1};
         char read_command[2] = {0xec, 0x05};
 
         i2c.write(addr, start_command, 2);
         ThisThread::sleep_for(BLINKING_RATE);
         i2c.write(addr, read_command, 2);
-        char value[6] = {0};
-        i2c.read(addr, value, 6);
+        char value[READ_BYTE_LEN] = {0};
+        i2c.read(addr, value, READ_BYTE_LEN);
 
         printf("i2c return : 0x%02X", value[0]);
-        for (int i = 0; i < 6; i++)
+        for (int i = 1; i < READ_BYTE_LEN; i++)
             printf("|0x%02X", value[i]);
-        printf(" -- counter:%d\n", counter);
+        printf(" -- counter:%d", counter);
+        uint16_t CO2 = 0;
+        float temp = 0.0;
+        float RH = 0.0;
+        decode_SCD4_read(value, &CO2, &temp, &RH);
+        printf(" -- CO2 ppm:%d|temp:%f°C|RH:%f%%\n", CO2,temp,RH);
+
         counter++;
         // led.write(button.read());
     }
