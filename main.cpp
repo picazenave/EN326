fc9182b083eacbd81befd26d138b5c6bf7a89ad9
Pierre Cazenave <47040310+picazenave@users.noreply.github.com>
2023-10-06 12:08:47 +0200

    sensor working, need decoding

--- main.cpp
+++ main.cpp
@@ -1,32 +1,49 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE 500ms
+#define BLINKING_RATE 5500ms
 
-int main()
-{
-    // Initialise the digital pin LED1 as an output
 #ifdef LED1
-    DigitalOut led(LED1);
+DigitalOut led(LED1);
 #else
 #error LED1 not defined
 #endif
 
 #ifdef BUTTON1
-    DigitalIn button(BUTTON1);
+DigitalIn button(BUTTON1);
+// InterruptIn button(BUTTON1);
 #else
 #error BUTTON1 not defined
 #endif
 
+I2C i2c(P1_I2C_SDA, P1_I2C_SCL);
+const char addr = (0x62 << 1);
+
+int counter = 0;
+
+void decodeSCD4(char *data, int *CO2ppm, float *temp, float *RH)
+{
+    *CO2ppm=data[0];
+}
+
+int main()
+{
     while (true)
     {
-        printf("aaaaaaaaaaaaaaaaa\n");
-        led.write(button.read());
-        // ThisThread::sleep_for(BLINKING_RATE);
+        char start_command[2] = {0x21, 0xb1};
+        char read_command[2] = {0xec, 0x05};
+
+        i2c.write(addr, start_command, 2);
+        ThisThread::sleep_for(BLINKING_RATE);
+        i2c.write(addr, read_command, 2);
+        char value[6] = {0};
+        i2c.read(addr, value, 6);
+
+        printf("i2c return : 0x%02X", value[0]);
+        for (int i = 0; i < 6; i++)
+            printf("|0x%02X", value[i]);
+        printf(" -- counter:%d\n", counter);
+        counter++;
+        // led.write(button.read());
     }
 }
